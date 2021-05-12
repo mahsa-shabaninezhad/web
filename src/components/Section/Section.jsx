d9d9d0f6e60ec65c335bf3b5e7ebb871b5031912
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-13 02:45:56 +0430

    Section component is created

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import './Section.css'
+
+
+const Section = ({headerText, children, theme=''}) => {
+    return (
+        <section className={`section ${theme}`}>
+            <h1 className="section-header">{headerText}</h1>
+            <div className="container">
+                {children}
+            </div>
+        </section>
+    )
+}
+
+Section.propTypes = {
+    headerText: PropTypes.string,
+    children: PropTypes.element,
+    theme: PropTypes.string
+}
+
+export default Section
