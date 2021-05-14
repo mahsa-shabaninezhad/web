a6929013fcf8b9b516272c8af879e6c2081fc5f4
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-14 23:50:43 +0430

    add menu toggle and active state for nav links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,25 +4,31 @@ import Logo from '../Logo/Logo'
 import './NavBar.css'
 
 const NavBar = () => {
-    const [toggle, setToggle]=useState('toggle toggle--hide')
-    
+    const [toggle, setToggle] = useState('toggle toggle--hide')
+    const [activeLink, setActiveLink] = useState('2')
+
+    const handleActiveLink = (e, id) => {
+        e.preventDefault()
+        setActiveLink(id)
+    }
+
     const handleToggleChange = () =>{
         toggle === 'toggle'?
         setToggle('toggle toggle--hide'):
         setToggle('toggle')
         
     }
-
+    
     return (
         <nav className="nav-bar">
             <Logo />
             <span className='toggle-icon' onClick={handleToggleChange}>&#9776;</span>
             <div className={toggle}>
-                <Link address='#' text='درباره ما'/>
-                <Link address='#' text='کسب و کار'/>
-                <Link address='#' text='پشتیبانی'/>
-                <Link address='#' text='سوالات متداول'/>
-                <Link address='#' text='فروشگاه من' theme='btn' />
+                <Link id="1" address='#' role="nav-link" activeItem={activeLink} onClick={handleActiveLink}>درباره ما</Link>
+                <Link id="2" address='#' role="nav-link" activeItem={activeLink} onClick={handleActiveLink}>کسب و کار</Link>
+                <Link id="3" address='#' role="nav-link" activeItem={activeLink} onClick={handleActiveLink}>پشتیبانی</Link>
+                <Link id="4" address='#' role="nav-link" activeItem={activeLink} onClick={handleActiveLink}>سوالات متداول</Link>
+                <Link id="5" address='#' role='btn'>فروشگاه من</Link>
             </div>
         </nav>
     )
