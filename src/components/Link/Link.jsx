fada5c499bc1a7b9bda0c5e75dda8a375b832e61
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-05-14 23:46:26 +0430

    modify Link to accept childern prop to wrap card component

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -2,18 +2,26 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import './Link.css'
 
-const Link = ({address, text, theme= ''}) => {
+const Link = ({address, children, role= '', activeItem = "0", onClick, id}) => {
+    let className = activeItem === id ? `${role} ${role}--active` : role
+
     return (
-        <a className={`nav-link ${theme}`} href={address}>
-            {text}
+        <a className={className} href={address} onClick={(e) => onClick(e,id)}>
+            {children}
         </a>
     )
 }
 
 Link.propTypes = {
     address: PropTypes.string,
-    text: PropTypes.string,
-    theme: PropTypes.string
+    role: PropTypes.oneOf(['card-link', 'nav-link', 'btn']),
+    children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element
+  ]),
+    activeItem: PropTypes.string,
+    id: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Link
